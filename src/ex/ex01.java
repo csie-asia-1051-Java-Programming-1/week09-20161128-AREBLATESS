bd1dbf5b1e697153682d0763a0d02bb98072586f
AREBLATESS <105021008@live.asia.edu.tw>
2016-12-16 08:19:14 +0800

    1

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -13,15 +13,22 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random rnd=new Random();
-		rnd.setSeed(23323456);
+		Random rnd=new Random(42);
+		Scanner scn=new Scanner(System.in);
+		int data[]=new int[6];
+		int n=scn.nextInt();
+		rnd.setSeed(n);
 		for(int i=0;i<6;i++){
-			System.out.print(rnd.nextInt(42)+1+"\t");
+			data[i]=rnd.nextInt(42)+1;
+			for(int j=0;j<=i;j++){
+				if(data[i]==data[j]){
+				data[i]=rnd.nextInt(42)+1;
+				}
+			}
 		}
-//		int[]data=new int[6];
-//		data[i]=
-//		System.out.print(data[i]=rnd.nextInt(42)+1);
-
+		for(int i=0;i<data.length;i++){
+			System.out.print(data[i]+" ");
+		}
 	}
 
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,30 @@
 /*
  * Topic: 讓使用者輸入一正整數 n，輸出 n 的所有正因數，並判斷該整數是否為質數
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021008 陳畯程
  */
-
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入一個數 我會幫你算出是否為質數");
+		int n=scn.nextInt();
+		int sum=0;
+		System.out.print("因數有");
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				System.out.print(i+" ");
+				sum++;
+			}
+		}
+		System.out.println();
+		if(sum<=2){
+			System.out.print("是質數");
+		}else{
+			System.out.print("不是質數");
+		}
 	}
 
 }
